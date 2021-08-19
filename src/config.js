03a1ff14005d5ea30cc3fc0ac87de6f70095bf3f
MuhammadHusayn <olimjonovm007@gmail.com>
2021-08-19 11:47:11 +0500

    improved auth modlue

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
+const ghToken = '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'
+const PRIVATE_KEY = 'olma'
 const host = 'localhost'
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4200
 
 export {
+	PRIVATE_KEY,
 	host,
 	PORT
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import express from 'express'
 import { host, PORT } from './config.js'
 const app = express()
+import authModule from './modules/auth/index.js'
+
+// third party global middlewares
+app.use( express.urlencoded({ extended: true }) )
+app.use( express.json() )
+
+// loding modules
+app.use( authModule )
 
 app.listen( PORT,  () => {
 	console.log('Server is running on http://' + host + ':' + PORT)
